3469cddf4a71f7ee28c5de31dfa2cdc11ca3a513
Patryk Pawlina <p4trykpawlina@gmail.com>
2023-07-19 17:06:36 +0200

    lotto

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,85 @@
 const btnPlayRef = document.querySelector('#playBtn');
 const inputRefs = [...document.querySelectorAll('input[id^="digit"]')];
+const resultsRef = document.querySelector('.result');
+
 
 const isAllNotEmpty = (nodes) => nodes.every((node) => node.value.trim() !== '');
 const isIntegers = (nodes) => nodes.every((node) => Number.isInteger(Number(node.value)));
-btnPlayRef.addEventListener('click', function(event){
+const convertToIntegers = (nodes) => nodes.map((node) => parseInt(node.value));
+const isInRange = (digits) => digits.every((digit) => digit >= 1 && digit <= 49);
+const isNotRedundant = (digits) => new Set(digits).size === digits.length;
 
-    if (isAllNotEmpty(inputRefs)){
-        console.log('ok')
+const drawDigits = () => {
+    const numbers = [];
+
+    while (numbers.length < 6) {
+        const result = Math.round(Math.random() * 48 + 1);
+        if (!numbers.includes(result)) {
+            numbers.push(result);
+        }
+    }
+
+    return numbers;
+}
+
+const checkHits = (userDigits, drawnDigits) => {
+    const hits = [];
+
+    userDigits.forEach((digit) => {
+         if (drawnDigits.includes(digit)) {
+            hits.push(digit);
+        }
+    })
+    return hits;
+}
+
+const checkHitsPro = (userDigits, drawnDigits) => userDigits.filter((digit) => drawnDigits.includes(digit));
+
+const calculatePrize = (quantity) => {
+    switch (quantity) {
+        case 3:
+            return 24;
+        case 4:
+            return 170;
+        case 5:
+            return 3500;
+        case 6:
+            return 3000000;
+        default:
+            return 0
+    }
+};
+
+const showResults = (hits, drawnDigits) => {
+    let message= `Wylosowane liczby to ${drawnDigits.join(', ')}. `;
+    if(hits.length > 0) {
+        message += `Trafiłeś ${hits.length} razy, twoje liczby to ${hits.join(', ')}.`
+        message += `Hajs: ${calculatePrize(hits.length)}PLN.`
+    } else {
+        message += 'Nic nie wygrałeś, spróbuj jeszcze raz, a na pewno wygrasz!'
+    }
+    resultsRef.innerText = message;
+}
+
+btnPlayRef.addEventListener('click', function (event) {
+
+    if (isAllNotEmpty(inputRefs)) {
+        if (isIntegers(inputRefs)) {
+            const userDigits = convertToIntegers(inputRefs);
+            if (isInRange(userDigits)) {
+                if (isNotRedundant(userDigits)) {
+                    const drawnDigits = drawDigits();
+                    const hits = checkHits(userDigits, drawnDigits);
+                    showResults(hits, drawnDigits);
+                } else {
+                    console.log('Liczby ci się powtarzają ziomeczku')
+                }
+            } else {
+                console.log ('liczby są poza zakresem 1-49')
+            }
+        } else {
+            console.log('liczby nie są liczbami')
+        }
     } else {
         console.log('coś jest puste')
     }
